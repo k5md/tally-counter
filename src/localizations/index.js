47f333e7648ff35c98576f9d45e5f168e444ddcc
k5md <serg.dfc@yandex.ru>
2020-06-12 20:24:22 +0300

    prepare for i18n

---
+++ src/localizations/index.js
@@ -0,0 +1,30 @@
+/* eslint global-require: 0 */
+
+import * as RNLocalize from 'react-native-localize';
+
+const translations = [
+  {
+    languageTag: 'en',
+    displayName: 'English',
+    translation: require('./en.json'),
+  },
+  {
+    languageTag: 'ru',
+    displayName: 'Русский',
+    translation: require('./ru.json'),
+  },
+];
+
+const fallback = {languageTag: 'en', isRTL: false};
+const {languageTag} =
+  RNLocalize.findBestAvailableLanguage(
+    translations.map(item => item.languageTag),
+  ) || fallback;
+
+const {translation} = translations.find(
+  item => item.languageTag === languageTag,
+);
+
+const translate = string => translation[string];
+
+export {translate, translations};
