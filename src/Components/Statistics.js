1cb41392d43728cc7b3fb871d58a89ff25e8cf65
k5md <serg.dfc@yandex.ru>
2020-06-30 20:15:56 +0300

    fix statistics update

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -32,27 +32,31 @@ const styles = StyleSheet.create({
   },
 });
 
-const Statistics = ({ read, stats, counters }) => {
-  const now = new Date();
-  console.log('statistics rerender');
+const current = {
+  get date() {
+    console.log('REQUEST TIME');
+    return new Date();
+  },
+};
 
-  const selectableFrames = [
-    { title: '1d', window: [getPrevDay(now), now.getTime()] },
-    { title: '3d', window: [getPrevDay(now, 3), now.getTime()] },
-    { title: 'M', window: [getPrevMonth(now), now.getTime()] },
-    { title: 'Y', window: [getPrevYear(now), now.getTime()] },
-    { title: 'All', window: [0, now.getTime()] },
-  ];
+const selectableFrames = [
+  { title: '1d', window: () => [getPrevDay(current.date), current.date.getTime()] },
+  { title: '3d', window: () => [getPrevDay(current.date, 3), current.date.getTime()] },
+  { title: 'M', window: () => [getPrevMonth(current.date), current.date.getTime()] },
+  { title: 'Y', window: () => [getPrevYear(current.date), current.date.getTime()] },
+  { title: 'All', window: () => [0, current.date.getTime()] },
+];
+
+const Statistics = ({ read, stats, counters }) => {
   const [selectedFrame, selectFrame] = useState(selectableFrames[0]);
 
   const selectableIds = Object.values(counters).map(({ id, title }) => ({ id, title }));
   const [selectedId, selectId] = useState(selectableIds[0]);
 
   useEffect(() => {
-    read(selectedId.id, selectedFrame.window);
+    read(selectedId.id, selectedFrame.window());
   }, [read, selectedId, selectedFrame, counters]);
 
-  console.log(stats);
   const formattedStats = stats.map(({ id, value, date }) => {
     const dateObject = new Date(date);
     return {
