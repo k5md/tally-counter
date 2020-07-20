bacad3f866b9391f0666fac84c68fbaffd14474f
k5md <serg.dfc@yandex.ru>
2020-07-21 00:33:25 +0300

    add launch screen

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import SplashScreen from 'react-native-splash-screen';
 import configureStore from './store/configureStore';
 import { Routes } from './Routes';
 
 const { persistor, store } = configureStore();
 
-const App = () => (
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <Routes />
-    </PersistGate>
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Routes />
+      </PersistGate>
+    </Provider>
+  );
+};
 
 export default App;
